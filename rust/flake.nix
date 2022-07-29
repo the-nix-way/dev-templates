e566bd0822017e2c726f598fee294e59f711ff02
Luc Perkins <lucperkins@gmail.com>
2022-07-29 02:29:34 +0200

    Make Rust env more robust

--- rust/flake.nix
+++ rust/flake.nix
@@ -13,10 +13,16 @@
         overlays = [ (import rust-overlay) ];
 
         pkgs = import nixpkgs { inherit system overlays; };
+        inherit (pkgs) rust-bin;
 
-        rust = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        rust = if builtins.pathExists ./rust-toolchain.toml then
+          rust-bin.fromRustupToolchainFile ./rust-toolchain.toml
+        else if builtins.pathExists ./rust-toolchain then
+          rust-bin.fromRustupToolchainFile ./rust-toolchain
+        else
+          rust-bin.stable.latest.default;
 
-        helpers = with pkgs; [ openssl pkg-config ];
+        helpers = with pkgs; [ openssl pkgconfig ];
 
         inherit (pkgs) mkShell;
         inherit (pkgs.lib) optionals;
@@ -26,7 +32,9 @@
 
         devShells = {
           default = mkShell {
-            buildInputs = [ rust ] ++ helpers;
+            nativeBuildInputs = [ rust ];
+
+            buildInputs = helpers;
 
             shellHook = ''
               ${rust}/bin/cargo --version
