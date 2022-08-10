ada54b20ccc42e53edd8a6a3cc2b56a6a4006d07
Luc Perkins <lucperkins@gmail.com>
2022-08-10 16:25:16 +0300

    Run nixfmt

--- elixir/flake.nix
+++ elixir/flake.nix
@@ -10,8 +10,7 @@
         pkgs = import nixpkgs { inherit system; };
 
         inherit (pkgs)
-          darwin gigalixir inotify-tools libnotify mkShell
-          terminal-notifier;
+          darwin gigalixir inotify-tools libnotify mkShell terminal-notifier;
         inherit (pkgs.beam.packages.erlangR25) elixir elixir_ls;
         inherit (pkgs.darwin.apple_sdk.frameworks) CoreFoundation CoreServices;
         inherit (pkgs.lib) optionals;
@@ -23,8 +22,7 @@
       in {
         devShells = {
           default = mkShell {
-            buildInputs = [ elixir elixir_ls ] ++ linuxDeps
-              ++ darwinDeps;
+            buildInputs = [ elixir elixir_ls ] ++ linuxDeps ++ darwinDeps;
 
             shellHook = ''
               ${elixir}/bin/mix --version
