56f9dbc255c2f7c4dc2f933aeb6b3cac7049aa90
Luc Perkins <lucperkins@gmail.com>
2023-07-13 15:45:24 -0700

    Support current SDKs

--- pulumi/flake.nix
+++ pulumi/flake.nix
@@ -14,9 +14,33 @@
       devShells = forEachSupportedSystem ({ pkgs }: {
         default = pkgs.mkShell {
           packages = with pkgs; [
+            # Pulumi plus:
+            # pulumi-watch
+            # pulumi-analyzer-* utilities
+            # pulumi-language-* utilities
+            # pulumi-resource-* utilities
             pulumi-bin
+
+            # Python SDK
             python311
+
+            # Go SDK
+            go_1_20
+
+            # Node.js SDK
+            nodejs
+
+            # .NET SDK
+            dotnet-sdk_6
+
+            # Java SDK
+            jdk
+            maven
+
+            # Kubernetes
             kubectl
+
+            # Miscellaneous utilities
             jq
           ];
         };
