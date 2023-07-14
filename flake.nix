ba5f6a2fbd7c74fbbfdb3bc4945d5fbc5fd1d8db
Luc Perkins <lucperkins@gmail.com>
2023-07-13 17:26:57 -0700

    Remove flake-utils from root flake.nix

--- flake.nix
+++ flake.nix
@@ -1,13 +1,64 @@
 {
-  description =
-    "Ready-made templates for easily creating flake-driven environments";
+  description = "Ready-made templates for easily creating flake-driven environments";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-  };
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-  outputs = { self, flake-utils, nixpkgs }:
+  outputs = { self, nixpkgs }:
+    let
+      overlays = [
+        (final: prev:
+          let
+            exec = pkg: "${prev.${pkg}}/bin/${pkg}";
+          in
+          {
+            format = prev.writeScriptBin "format" ''
+              ${exec "nixpkgs-fmt"} **/*.nix
+            '';
+            dvt = prev.writeScriptBin "dvt" ''
+              if [ -z $1 ]; then
+                echo "no template specified"
+                exit 1
+              fi
+
+              TEMPLATE=$1
+
+              ${exec "nix"} \
+                --experimental-features 'nix-command flakes' \
+                flake init \
+                --template \
+                "github:the-nix-way/dev-templates#''${TEMPLATE}"
+            '';
+            update = prev.writeScriptBin "update" ''
+              for dir in `ls -d */`; do # Iterate through all the templates
+                (
+                  cd $dir
+                  ${exec "nix"} flake update # Update flake.lock
+                  ${exec "nix"} flake check  # Make sure things work after the update
+                )
+              done
+            '';
+          })
+      ];
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit overlays system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [ format update ];
+        };
+      });
+
+      packages = forEachSupportedSystem ({ pkgs }: rec {
+        default = dvt;
+        inherit (pkgs) dvt;
+      });
+    }
+
+    //
+
     {
       templates = rec {
         clojure = {
@@ -153,52 +204,5 @@
         # Aliases
         rt = rust-toolchain;
       };
-    } // flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        inherit (pkgs) mkShell writeScriptBin;
-        exec = pkg: "${pkgs.${pkg}}/bin/${pkg}";
-
-        format = writeScriptBin "format" ''
-          ${exec "nixpkgs-fmt"} **/*.nix
-        '';
-
-        dvt = writeScriptBin "dvt" ''
-          if [ -z $1 ]; then
-            echo "no template specified"
-            exit 1
-          fi
-
-          TEMPLATE=$1
-
-          ${exec "nix"} \
-            --experimental-features 'nix-command flakes' \
-            flake init \
-            --template \
-            "github:the-nix-way/dev-templates#''${TEMPLATE}"
-        '';
-
-        update = writeScriptBin "update" ''
-          for dir in `ls -d */`; do # Iterate through all the templates
-            (
-              cd $dir
-              ${exec "nix"} flake update # Update flake.lock
-              ${exec "nix"} flake check  # Make sure things work after the update
-            )
-          done
-        '';
-      in
-      {
-        devShells = {
-          default = mkShell {
-            packages = [ format update ];
-          };
-        };
-
-        packages = rec {
-          default = dvt;
-
-          inherit dvt;
-        };
-      });
+    };
 }
--- purescript/flake.nix
+++ purescript/flake.nix
@@ -11,7 +11,7 @@
 
   outputs = { self, nixpkgs, easy-purescript-nix }:
     let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ]; # "aarch64-linux" not supported
       forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
         pkgs = import nixpkgs { inherit system; };
       });
