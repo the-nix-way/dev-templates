ca3d0b7853b7032a9cc0660121a0aef4bf71c471
g0dm0d <ivan.berlin06@gmail.com>
2023-12-28 00:04:33 +0300

    fix purescript

--- purescript/flake.nix
+++ purescript/flake.nix
@@ -2,7 +2,7 @@
   description = "A Nix-flake-based Purescript development environment";
 
   inputs = {
-    inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
     easy-purescript-nix = {
       url = "github:justinwoo/easy-purescript-nix";
       flake = false;
