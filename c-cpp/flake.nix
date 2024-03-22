cf5b99113e82b1b8c18f2e28ac466229afe52cf0
Joel Benway <157863269+joelbenway@users.noreply.github.com>
2024-03-22 14:27:36 -0500

    Add C/C++ development flake with some sane defaults.

--- c-cpp/flake.nix
+++ c-cpp/flake.nix
@@ -24,19 +24,11 @@
             conan
             cppcheck
             doxygen
-<<<<<<< HEAD
-=======
-            gdb
->>>>>>> 66cbf13 (Add C/C++ development flake with some sane defaults.)
             gtest
             lcov
             vcpkg
             vcpkg-tool
-<<<<<<< HEAD
           ] ++ (if system == "aarch64-darwin" then [ ] else [ gdb ]);
-=======
-          ];
->>>>>>> 66cbf13 (Add C/C++ development flake with some sane defaults.)
         };
       });
     };
