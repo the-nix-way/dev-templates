b791b80e2a2e1cd07179b9f6f88dd9a0e4a4ec70
argonarch <armando12123434@gmail.com>
2024-09-05 18:12:24 -0300

    add platformio framework

---
+++ platformio/flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "A Nix-flake-based C/C++ development environment";
+
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+    forEachSupportedSystem = f:
+      nixpkgs.lib.genAttrs supportedSystems (system:
+        f {
+          pkgs = import nixpkgs {inherit system;};
+        });
+  in {
+    devShells = forEachSupportedSystem ({pkgs}: {
+      default = pkgs.mkShell {
+        shellHook = ''
+          export PLATFORMIO_CORE_DIR=$PWD/.platformio
+        '';
+        buildInputs = with pkgs; [
+          platformio
+        ];
+        packages = with pkgs;
+          [
+            clang-tools
+            cmake
+            codespell
+            conan
+            cppcheck
+            doxygen
+            gtest
+            lcov
+            vcpkg
+            vcpkg-tool
+          ]
+          ++ (
+            if system == "aarch64-darwin"
+            then []
+            else [gdb]
+          );
+      };
+    });
+  };
+}
