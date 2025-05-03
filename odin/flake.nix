158f84d51d657a32a81c71780576b5a37949e6cf
Luc Perkins <lucperkins@gmail.com>
2025-05-03 15:12:56 -0300

    Reformat Odin flake

--- odin/flake.nix
+++ odin/flake.nix
@@ -3,18 +3,12 @@
 
   inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
 
-  outputs =
-    { self
-    , nixpkgs
-    ,
-    }:
+  outputs = { self, nixpkgs }:
     let
       supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f:
-        nixpkgs.lib.genAttrs supportedSystems (system:
-          f {
-            pkgs = import nixpkgs { inherit system; };
-          });
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
     in
     {
       devShells = forEachSupportedSystem ({ pkgs }: {
