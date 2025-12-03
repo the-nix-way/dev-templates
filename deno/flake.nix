cbc6c1801e169ea28ead23e591366bf35fb80669
jeandavilarodz <33385948+jeandavilarodz@users.noreply.github.com>
2025-12-02 21:04:35 -0800

    Adding a deno flake template

---
+++ deno/flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "A Nix-flake-based Deno development environment";
+
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1"; # unstable Nixpkgs
+
+  outputs =
+    { self, ... }@inputs:
+
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        inputs.nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import inputs.nixpkgs { inherit system; };
+          }
+        );
+    in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShellNoCC {
+            packages = with pkgs; [ deno ];
+          };
+        }
+      );
+    };
+}
