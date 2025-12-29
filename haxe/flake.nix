0caea070d50be98358e2be6af01a5bdf70f6342d
Sebastián Zavala Villagómez <sebaszv@proton.me>
2025-12-28 19:36:43 -0500

    haxe: Use `haxe_4_0` instead of `haxe` by default
    This is to get around `mbedtls_2` being marked as insecure, which is
    required for Haxe versions >=4.1. The reality is that this is failing the
    PR checks, which is annoying, so this solves the issue for the moment.
    There is a comment explaining this so the user can take action as they
    see fit until the upstream expression is updated.

--- haxe/flake.nix
+++ haxe/flake.nix
@@ -27,7 +27,17 @@
         { pkgs }:
         {
           default = pkgs.mkShellNoCC {
-            packages = with pkgs; [ haxe ];
+            # WARN: As of 2025-12-28:
+            #       `haxe_4_0` is explicitly used because
+            #       `haxe` is an alias for `haxe_4_3`,
+            #       which requires `mbedtls_2`, which has
+            #       been marked as insecure. The build
+            #       expression for Haxe has not been
+            #       updated to handle this yet, so it fails
+            #       to build. This can be changed in the
+            #       future or by the user, such as through
+            #       an override.
+            packages = with pkgs; [ haxe_4_0 ];
           };
         }
       );
